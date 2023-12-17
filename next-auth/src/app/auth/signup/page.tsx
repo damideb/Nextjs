3acdf12a0ccb03ae48a565b8387e1ab328a9107c
Damilola Adebowale <adebzdamzy@gmail.com>
2023-12-17 19:30:46 +0100

    firebase signin

--- next-auth/src/app/auth/signup/page.tsx
+++ next-auth/src/app/auth/signup/page.tsx
@@ -1,25 +1,32 @@
 'use client'
 import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 import {auth} from '@/app/firebase'
-import { useRouter} from "next/navigation"
-import { signIn, } from "next-auth/react"
-import Link from "next/link"
+import { useRouter } from "next/navigation"
+
 
 export default function Signin() {
 
     
-    const[useremail, setUseremail] = useState('')
-    const [userPassword, setUserpassword] = useState('')
+const[userInfo, setUserInfo] = useState({
+    email: '',
+    password: ''
 
+})
 
 const router = useRouter()
 
+const signUp =()=>{
+    createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+    router.push("/")
+   
+}
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+              Register your account
             </h2>
           </div>
   
@@ -33,7 +40,7 @@ const router = useRouter()
                   <input
                     id="email"
                     name="email"
-                    onChange={(e)=>setUseremail(e.target.value)}
+                    onChange={(e)=>setUserInfo({...userInfo, email:e.target.value})}
                     type="email"
                     autoComplete="email"
                     required
@@ -58,7 +65,7 @@ const router = useRouter()
                     id="password"
                     name="password"
                     type="password"
-                    onChange={(e)=>setUserpassword(e.target.value)}
+                    onChange={(e)=>setUserInfo({...userInfo, password:e.target.value})}
                     autoComplete="current-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -70,18 +77,18 @@ const router = useRouter()
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  onClick={()=>signIn('credentials', {useremail, userPassword, redirect:true, callbackUrl:"/"})}
+                  onClick={()=>signUp()}
                 >
-                  Sign in
+                  Sign Up
                 </button>
               </div>
             </div>
   
             <p className="mt-10 text-center text-sm text-gray-500">
               Not a member?{' '}
-             <span className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300"> 
-                <Link href="/auth/signup">click here to register your account</Link>
-            </span>
+              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Start a 14 day free trial
+              </a>
             </p>
           </div>
         </div>
--- next-auth/src/app/components/Navbar.tsx
+++ next-auth/src/app/components/Navbar.tsx
@@ -5,7 +5,8 @@ export default function Navbar() {
         <nav className="bg-blue-800 p-4">
             <ul className="flex justify-evenly text-2xl font-bold">
                 <li><Link href="/">Home</Link></li>
-                <li><Link href="/api/auth/signin">Sign In</Link></li>
+                <li><Link href="/auth/signup">Sign Up</Link></li>
+                <li><Link href="/auth/signin">Sign In</Link></li>
                 <li><Link href="/api/auth/signout">Sign Out</Link></li>
                 <li><Link href="/server">Server</Link></li>
                 <li><Link href="/client">Client</Link></li>
